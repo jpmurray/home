93b1e069a66c16c386e3b41693f9a7ba7541e704
Jean-Philippe Murray <jeanphilippe@pasunecompagnie.com>
2018-12-20 14:18:21 -0500

    Links and relationship with users

---
+++ database/migrations/2018_12_20_190308_create_links_table.php
@@ -0,0 +1,39 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateLinksTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('links', function (Blueprint $table) {
+            $table->increments('id');
+            $table->string('name');
+            $table->text('url');
+            $table->timestamps();
+        });
+
+        Schema::create('link_user', function (Blueprint $table) {
+            $table->integer('link_id');
+            $table->integer('user_id');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('links');
+        Schema::dropIfExists('link_user');
+    }
+}
